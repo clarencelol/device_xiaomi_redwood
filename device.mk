962cdd2c48dc2a553fc399a696710e9ebf081567
clarencelol <clarencekuiek@proton.me>
2024-03-04 12:47:53 +0800

    redwood: Import powerhint.json from Spacewar
    * https://github.com/LineageOS/android_device_nothing_Spacewar/blob/lineage-20/configs/perf/powerhint.json
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- device.mk
+++ device.mk
@@ -443,6 +443,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_COPY_FILES += \
