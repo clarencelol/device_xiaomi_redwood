054de022ce7e79af6b04b8acf8c3361db4fd1b21
clarencelol <clarencekuiek@proton.me>
2024-03-14 22:42:28 +0800

    redwood: Switch to Scarlet-X
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,8 +94,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := TARGET_PRODUCT=$(PRODUCT_DEVICE)
 TARGET_KERNEL_NO_GCC := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/redwood
-TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/redwood_QGKI.config
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7325
+TARGET_KERNEL_CONFIG := vendor/xiaomi-qgki_defconfig vendor/redwood.config
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
@@ -113,7 +113,6 @@ BOARD_KERNEL_CMDLINE += kpti=off
 
 # Kernel modules
 BOOT_KERNEL_MODULES := \
-    goodix_core.ko \
     xiaomi_touch.ko
 
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
